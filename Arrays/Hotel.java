cdeaf2f3ff1cda6f2b8b86a62124f63816b69b71
Alok Jadhao <jadhaoalok51@gmail.com>
2024-02-28 16:22:29 +0530

    Added comments and indentation

--- Arrays/Hotel.java
+++ Arrays/Hotel.java
@@ -1,28 +1,33 @@
 package Arrays;
 
 public class Hotel {
-    private int hotelId;
-    private String location;
-    private int[] rooms; // Array format: [suites, deluxe, standard]
+    private int hotelId; // ID of the hotel
+    private String location; // Location of the hotel
+    private int[] rooms; // Array format: [suites, deluxe, standard] - number of each type of room
 
+    // Constructor to initialize hotel details
     public Hotel(int hotelId, String location, int[] rooms) {
         this.hotelId = hotelId;
         this.location = location;
         this.rooms = rooms;
     }
 
+    // Getter method for hotelId
     public int getHotelId() {
         return hotelId;
     }
 
+    // Getter method for location
     public String getLocation() {
         return location;
     }
 
+    // Getter method for rooms array
     public int[] getRooms() {
         return rooms;
     }
 
+    // Method to list hotels in a given location
     public static void listHotels(Hotel[] hotels, String location) {
         System.out.println("Hotels in " + location + ":");
         for (Hotel hotel : hotels) {
@@ -32,14 +37,16 @@ public class Hotel {
         }
     }
 
+    // Method to list total number of rooms of a certain type in each hotel
     public static void listHotels(Hotel[] hotels, int roomType) {
         String[] roomTypes = {"Suite", "Deluxe", "Standard"};
-        System.out.println("Total number of rooms of type " + roomTypes[roomType] + " in each hotel:");
+        System.out.println("Total number of " + roomTypes[roomType] + " rooms in each hotel:");
         for (Hotel hotel : hotels) {
             System.out.println("Hotel ID: " + hotel.getHotelId() + ", Location: " + hotel.getLocation() + ", " + roomTypes[roomType] + " rooms: " + hotel.getRooms()[roomType]);
         }
     }
 
+    // Helper method to convert an array to string
     private static String arrayToString(int[] array) {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
@@ -53,6 +60,7 @@ public class Hotel {
         return stringBuilder.toString();
     }
 
+    // Main method to demonstrate class functionality
     public static void main(String[] args) {
         // Creating hotels
         Hotel[] hotels = new Hotel[3];
@@ -67,4 +75,3 @@ public class Hotel {
         Hotel.listHotels(hotels, 1); // Type 1 represents "Deluxe" rooms
     }
 }
-
--- Arrays/MovieSort.java
+++ Arrays/MovieSort.java
@@ -1,122 +1,87 @@
-/*
- * Write a program in Java to maintain the information of Movies which includes the information of name
-of movie , type of movie( action , thriller , comedy ,drama ) , Hero name , Heroine , budget in Rs. Write
-a program to accept the information of movies from user and sort them according to the budget of the
-film.
- */
-
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Information{
-	String name;
-	String movietype;
-	String hero,heroine;
-	int budget;
+// Class to hold information about movies
+class Information {
+    String name;
+    String movietype;
+    String hero, heroine;
+    int budget;
 
-	public Information(){
+    // Default constructor
+    public Information() {
 
-	}
+    }
 
-	public Information(String name, String movietype, String hero, String heroine, int budget) {
-		this.name = name;
-		this.movietype = movietype;
-		this.hero = hero;
-		this.heroine = heroine;
-		this.budget = budget;
-	}
+    // Parameterized constructor
+    public Information(String name, String movietype, String hero, String heroine, int budget) {
+        this.name = name;
+        this.movietype = movietype;
+        this.hero = hero;
+        this.heroine = heroine;
+        this.budget = budget;
+    }
 
-	public void display(){
-		System.out.println("The name of the movie is: " + name);
-		System.out.println("The movie type is: " + movietype);
-		System.out.println("The name of the hero is: " + hero);
-		System.out.println("The name of the heroine is: " + heroine);
-		System.out.println("The budget of the movie is: " + budget);
-		
-	}
-
-	
-	
+    // Method to display movie details
+    public void display() {
+        System.out.println("The name of the movie is: " + name);
+        System.out.println("The movie type is: " + movietype);
+        System.out.println("The name of the hero is: " + hero);
+        System.out.println("The name of the heroine is: " + heroine);
+        System.out.println("The budget of the movie is: " + budget);
+    }
 }
 
-public class MovieSort{
-	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		String name;
-		String movietype;
-		String hero,heroine;
-		int budget;
-		
-		
-		System.out.print("Enter number of Movies: ");
-		int n = scanner.nextInt();
+public class MovieSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String movietype;
+        String hero, heroine;
+        int budget;
 
-		
-		Information[] movies= new Information[n];
+        // Accepting the number of movies from the user
+        System.out.print("Enter number of Movies: ");
+        int n = scanner.nextInt();
 
+        // Array to hold movie objects
+        Information[] movies = new Information[n];
 
-		for (int i = 0; i < n; i++) {
-		
-			System.out.println("Enter the details for movie " + (i+1));
-			System.out.print("Please enter the movie name: ");
-			scanner.nextLine();
-			name = scanner.nextLine();	
-			System.out.print("Please enter the movie type: ");
-			movietype = scanner.nextLine();
-			System.out.print("Please enter the hero name: ");
-			hero = scanner.nextLine();
-			System.out.print("Please enter the heroine name: ");
-			heroine = scanner.nextLine();
-			System.out.print("Please enter the movie budget in cr: ");
-			budget = scanner.nextInt();
-			
-			
-		 	System.out.println();
-			
-			movies[i] = new Information(name, movietype, hero, heroine, budget);
-		}
+        // Accepting movie details from the user
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the details for movie " + (i + 1));
+            System.out.print("Please enter the movie name: ");
+            scanner.nextLine(); // Consume newline left by previous nextInt()
+            name = scanner.nextLine();
+            System.out.print("Please enter the movie type: ");
+            movietype = scanner.nextLine();
+            System.out.print("Please enter the hero name: ");
+            hero = scanner.nextLine();
+            System.out.print("Please enter the heroine name: ");
+            heroine = scanner.nextLine();
+            System.out.print("Please enter the movie budget in cr: ");
+            budget = scanner.nextInt();
 
-		Information temp = new Information();
-		for (int i = 0; i < movies.length; i++) {
-			for (int j = 1; j < movies.length-i; j++) {
-				if(Double.compare(movies[j].budget, movies[j-1].budget) < 0){
-					
-					temp = movies[j];
-					movies[j] = movies[j-1];
-					movies[j-1] = temp;
-				}
-			}
-		}
+            System.out.println();
 
-		System.out.println("Details of highest budget movie are: ");
-		movies[n-1].display();
+            // Creating Information object and storing it in the movies array
+            movies[i] = new Information(name, movietype, hero, heroine, budget);
+        }
 
-		// arr[j] = arr[j-1];
-		// arr[j-1] = temp;
+        // Sorting movies based on budget
+        Information temp;
+        for (int i = 0; i < movies.length; i++) {
+            for (int j = 1; j < movies.length - i; j++) {
+                if (movies[j].budget < movies[j - 1].budget) {
+                    temp = movies[j];
+                    movies[j] = movies[j - 1];
+                    movies[j - 1] = temp;
+                }
+            }
+        }
 
-		// for (int i = 0; i < movies.length; i++) {
-		// 	System.out.println(movies[i].budget);
-		// }
-
-		
-		// movies[j].budget.compareTo
-
-		scanner.close();
-	}
-
-	// Arrays.sort(movies);
-		//I want to sort the object array based on the budget attribute.
-		//Syntax: Arrays.sort(array);
-		// void budgetSort() {
-		// 	for (int i = 0; i < movies.length; i++) {
-		// 		for (int j = 0; j < movies.length-1; j++) {
-		// 			if(Double.compare(movies[j].budget, movies[j+1].budget) > 0){
-		// 				Information temp = new Information();
-		// 				temp = movies[j];
-		// 				movies[j] = movies[j+1];
-		// 				movies[j+1] = movies[j];
-		// 			}
-		// 		}
-		// 	}
-		// }
+        // Displaying details of the movie with highest budget
+        System.out.println("Details of highest budget movie are: ");
+        movies[n - 1].display();
+        scanner.close();
+    }
 }
