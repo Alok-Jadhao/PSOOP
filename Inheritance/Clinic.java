b9d5f5e960872f89ebba324ef183900e10b0b2f3
Alok Jadhao <144699874+Alok-Jadhao@users.noreply.github.com>
2024-03-12 09:56:23 +0530

    Create Clinic.java

---
+++ Inheritance/Clinic.java
@@ -0,0 +1,71 @@
+/*
+Problem Statement#3
+
+Give the definition of four classes, Person, Doctor, Patient and Billing, whose objects are records for a clinic. 
+Class Doctor will be derived from the class Person. A doctor have name and Date  (inherited from the class Person), it’s speciality; fees and income;
+Patient will be derived from the class Person. A Patient record has the patient’s name and Date  (inherited from the class Person) and a Doctor object.
+A Billing object will contain a Patient object, a Doctor object, Date of bill using date object and an amount due of type double. Be sure your classes have a reasonable complement of constructors, copy constructor, override equals(check equality of object)  and toString methods and member functions. 
+First write a driver program to test all your member functions, and then write a test program that creates at least two patients, at least two doctors, and at least two Billing records, then prints out the total income from the Billing records.
+At the time of Billing the Patient’s Doctor  name and Billing object’s doctor name equality should be checked.
+Total bill generated will be no of days the patient admitted ( current date-admitted date ) * 2000+ doctore fees * no of days from the date of billing ( current date-admitted date ).
+Update the Doctors income to no of days from the date of billing ( current date-admitted date ).
+*/
+import java.util.*;
+
+class Person {
+    String name;
+    Date date; //joining date
+
+    public Person(String name, Date date) {
+        this.name = name;
+        this.date = date;
+    }
+}
+
+class Doctor extends Person {
+    String speciality;
+    double fees;
+    double income;
+
+    public Doctor(String name, Date date, String speciality, double fees, double income) {
+        super(name, date);
+        this.speciality = speciality;
+        this.fees = fees;
+        this.income = income;
+    }
+}
+
+class Patient extends Person {
+    Doctor doctor;
+
+    public Patient(String name, Date date, Doctor doctor) {
+        super(name, date);
+        this.doctor = doctor;
+    }
+}
+
+class Billing {
+    Patient patient;
+    Doctor doctor;
+    Date last_date; //leaving date
+    double amountDue;
+
+    public Billing(Patient patient, Doctor doctor, Date last_Date, double amountDue) {
+        this.patient = patient;
+        this.doctor = doctor;
+        this.last_date = last_Date;
+        this.amountDue = amountDue;
+    }
+}
+
+public class Clinic {
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        Date joinDate = new Date(2024,2,1);
+        //leaving date and person date are
+    
+        Date releaseDate = new Date(2024, 3, 1);
+
+        
+    }
+}
