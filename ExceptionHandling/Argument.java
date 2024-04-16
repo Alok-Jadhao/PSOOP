de8c7ddcf74152c70e5066908e76c5d0e764cc7f
Alok Jadhao <144699874+Alok-Jadhao@users.noreply.github.com>
2024-04-16 10:54:16 +0530

    Exception-Hanlding

---
+++ ExceptionHandling/Argument.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Argument {
+	public static void main(String[] args) {
+		System.out.println("Enter two integers:");
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		int c = Integer.parseInt(args[0]);
+		System.out.print(a/b);
+	}
+}
---
+++ ExceptionHandling/StringExcep.java
@@ -0,0 +1,7 @@
+public class StringExcep {
+	public static void main(String[] args) {
+		String ptr = null;
+
+		System.out.println(ptr.length());
+	}
+}
